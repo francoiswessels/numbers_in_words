1d97448f18a5b47a4e566bea3729a5c2266ce406
francoiswessels <francoisw@gmail.com>
2020-06-13 15:26:09 +0200

    evaluated some approaches. settled on one.

--- main.py
+++ main.py
@@ -1,35 +1,31 @@
 
+from typing import List, Dict, Union
 import timeit
+import numbers_in_words as niw
 
-
-setup = """
-from typing import List,Union
-import num_in_words as niw
 
 class Scenario:
     def __init__(self, input_value: str, expected_value: Union[str, None]):
         self.input_value = input_value
         self.expected_value = expected_value
 
+
 number_scenarios: List[Scenario] = [
-            Scenario("The pump is 536 deep underground.", "536"),
-            Scenario("We processed 9121 records.", "9121"),
-            Scenario("Variables reported as having a number invalid missing type #65678.", None),
-            Scenario("Interactive and printable 10022 ZIP code.", "10022"),
-            Scenario("The database has 66723107008 records.", "66723107008"),
-            Scenario("I received 23 456,9 KGs.", None)]"""
-
-s1 = """for i in range(1,10000000,1):    
-    (niw.number_in_words_from_phrase(str(i)))"""
-
-s2 = """for i in range(1,10000000,1):    
-    (niw.number_in_words_from_phrase_2(str(i)))"""
+    Scenario("The pump is 536 deep underground.", "536"),
+    Scenario("We processed 9121 records.", "9121"),
+    Scenario("Variables reported as having a number invalid missing type #65678.", None),
+    Scenario("Interactive and printable 10022 ZIP code.", "10022"),
+    Scenario("The database has 66723107008 records.", "66723107008"),
+    Scenario("I received 23 456,9 KGs.", None)]
 
 
-print(timeit.timeit(stmt=s1, setup=setup, number=1))
-print(timeit.timeit(stmt=s2, setup=setup, number=1))
+# for s in number_scenarios:
+#     print(s.expected_value, niw.number_in_words_from_phrase(s.input_value))
 
+statement1 = "niw.number_in_words_from_phrase('999', cached=False)"
+statement2 = "niw.number_in_words_from_phrase('99999999999999999999', cached=True)"
 
+setup = "import numbers_in_words as niw"
 
-
-    
+print(timeit.timeit(stmt=statement1, setup=setup, number=100000))
+print(timeit.timeit(stmt=statement2, setup=setup, number=100000))
--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 from typing import List, Union
 import unittest as ut
 
-import num_in_words as niw
+import numbers_in_words as niw
 
 
 class TestModuleAPI(ut.TestCase):
     def setUp(self):
         self.public_members = [m for m in dir(niw) if not m[0] == "_"]
-        self.expected_members = ["number_in_words", "number_in_words_from_phrase", "number_in_words_2", "number_in_words_from_phrase_2"]
+        self.expected_members = [
+                        "number_in_words",
+                        "number_in_words_from_phrase"]
 
     def test_expected_public_module_members(self):
         for em in self.expected_members:
@@ -15,14 +17,14 @@ class TestModuleAPI(ut.TestCase):
 
     def test_no_unexpected_public_module_members(self):
         other_members = [m for m in self.public_members if m not in self.expected_members]
-        self.assertFalse(any(other_members), msg=f"Found unexpected public members in module:\n{other_members}")
+        self.assertFalse(any(other_members), msg=f"Found unexpected public members in module:\n\t{other_members}")
 
 
 class TestValueMapComplete(ut.TestCase):
     def test_map_members_complete(self):
-        expected_keys = [i for i in range(0,20)]
-        expected_keys += [i*10 for i in range(2,10)]
-        mapped_keys = niw._map_to_words.keys()
+        expected_keys = [i for i in range(0, 20)]
+        expected_keys += [i*10 for i in range(2, 10)]
+        mapped_keys = niw._num_to_words_map.keys()
         outcomes = [ek in mapped_keys for ek in expected_keys]
         self.assertTrue(all(outcomes), msg=outcomes)
 
@@ -36,13 +38,25 @@ class Scenario:
 class TestProvidedTestCases(ut.TestCase):
     def setUp(self):
         self.full_scenarios: List[Scenario] = [
-            Scenario("The pump is 536 deep underground.", "five hundred and thirty-six"),
-            Scenario("We processed 9121 records.", "nine thousand, one hundred and twenty-one"),
-            Scenario("Variables reported as having a number invalid missing type #65678.", "number invalid"),
-            Scenario("Interactive and printable 10022 ZIP code.", "ten thousand and twenty-two"),
-            Scenario("The database has 66723107008 records.", "sixty-six billion, seven hundred and twenty-three million, one hundred and seven thousand and eight"),
-            Scenario("I received 23 456,9 KGs.", "number invalid")]
-        
+            Scenario(
+                "The pump is 536 deep underground.",
+                "five hundred and thirty-six"),
+            Scenario(
+                "We processed 9121 records.",
+                "nine thousand, one hundred and twenty-one"),
+            Scenario(
+                "Variables reported as having a number invalid missing type #65678.",
+                "number invalid"),
+            Scenario(
+                "Interactive and printable 10022 ZIP code.",
+                "ten thousand and twenty-two"),
+            Scenario(
+                "The database has 66723107008 records.",
+                "sixty-six billion, seven hundred and twenty-three million, one hundred and seven thousand and eight"),
+            Scenario(
+                "I received 23 456,9 KGs.",
+                "number invalid")]
+
         self.number_scenarios: List[Scenario] = [
             Scenario("The pump is 536 deep underground.", "536"),
             Scenario("We processed 9121 records.", "9121"),
@@ -61,18 +75,6 @@ class TestProvidedTestCases(ut.TestCase):
             outcome = niw.number_in_words_from_phrase(scenario.input_value)
             self.assertEqual(outcome, scenario.expected_value, msg=f"Output incorrect for '{scenario.input_value}'")
 
-    def test_provided_scenarios_2(self):
-        for scenario in self.full_scenarios:
-            outcome = niw.number_in_words_from_phrase_2(scenario.input_value)
-            self.assertEqual(outcome, scenario.expected_value, msg=f"Output incorrect for '{scenario.input_value}'")
-
-
-class TestByComparingTwoApproaches(ut.TestCase):
-    def test_a_range(self):
-        for i in range(100000000):
-            v1 = niw.number_in_words(str(i))
-            v2 = niw.number_in_words_2(str(i))
-            self.assertEqual(v1, v2)
 
 if __name__ == "__main__":
     ut.main()
